4d04ec17d6ff5d223b9d74b838fd101245fa119e
Joares <joaresmiranda@gmail.com>
2022-10-20 00:13:10 -0400

    update build

--- docs/assets/index.458bf0f8.js
+++ docs/assets/index.458bf0f8.js
@@ -37,7 +37,7 @@
             type="button"
             class="button w-full bg-red-600 p-4 rounded-md text-white lowercase"
           >
-            Exchange
+            Check values
           </button>
         </form>
       </article>
@@ -57,12 +57,12 @@
       </article>
 
     </main>
-`;const P=.13,x=.15,T=3.9,B=document.querySelector("#priceForm"),d=document.querySelector("#inputPrice"),i=document.querySelector("#haveTax"),g=document.querySelector("#haveTip"),y=document.querySelector("#inputPriceBr"),C=document.querySelector("#exchangeButton"),k=document.querySelector("#resultContainer"),$=document.querySelector(".resultPrices"),L=document.querySelector("#backButton"),f=e=>parseFloat(e)*P,m=e=>parseFloat(e)+parseFloat(f(e)),u=(e,t)=>t?parseFloat(m(e)*x):parseFloat(e)*x,b=(e,t)=>parseFloat(t?m(e)+u(e,t):e+u(e,t)),a=e=>parseFloat(e)*T,S=(e,t,n)=>{let c=e;return t&&n?c=a(b(c,!0)):t?c=a(e+f(c)):n?c=a(e+u(c)):a(c)},p=new Intl.NumberFormat("pt-BR",{style:"currency",currency:"BRL"}),l=new Intl.NumberFormat("en-CA",{style:"currency",currency:"CAD"}),v=()=>{B.classList.toggle("hidden"),k.classList.toggle("hidden")};d.focus();C.addEventListener("click",e=>{const t=parseFloat(d.value.replace(",","."));if(isNaN(t))return alert("Please, insert a price in CAD");const n=S(t,i.checked,g.checked),c=i.checked?`
+`;const P=.13,x=.15,T=3.9,B=document.querySelector("#priceForm"),d=document.querySelector("#inputPrice"),i=document.querySelector("#haveTax"),g=document.querySelector("#haveTip"),y=document.querySelector("#inputPriceBr"),C=document.querySelector("#exchangeButton"),k=document.querySelector("#resultContainer"),$=document.querySelector(".resultPrices"),L=document.querySelector("#backButton"),f=e=>parseFloat(e)*P,m=e=>parseFloat(e)+parseFloat(f(e)),u=(e,t)=>t?parseFloat(m(e)*x):parseFloat(e)*x,v=(e,t)=>parseFloat(t?m(e)+u(e,t):e+u(e,t)),a=e=>parseFloat(e)*T,S=(e,t,n)=>{let c=e;return t&&n?c=a(v(c,!0)):t?c=a(e+f(c)):n?c=a(e+u(c)):a(c)},p=new Intl.NumberFormat("pt-BR",{style:"currency",currency:"BRL"}),l=new Intl.NumberFormat("en-CA",{style:"currency",currency:"CAD"}),b=()=>{B.classList.toggle("hidden"),k.classList.toggle("hidden")};d.focus();C.addEventListener("click",e=>{const t=parseFloat(d.value.replace(",","."));if(isNaN(t))return alert("Please, insert a price in CAD");const n=S(t,i.checked,g.checked),c=i.checked?`
     <li>\u{1F4B8} tax: CAD ${l.format(f(t))}</li>
     <li>\u{1F4B5} price + tax: CAD ${l.format(m(t))}</li>`:`
     <li>\u26A0\uFE0F <span class="text-xs text-slate-600">Price without taxes</span></li>`,r=g.checked?`
     <li>\u{1F3A9} tip: CAD ${l.format(u(t,i.checked))}</li>
-    <li>\u{1FAE5} final price: CAD ${l.format(b(t,i.checked))}`:"",o=parseFloat(y.value.replace(",",".")),s=o-n,h=((1-n/o)*100).toFixed(2),w=h>0?"\u2705 Good choice":"\u{1F6AB} Bad choice",F=o?`
+    <li>\u{1FAE5} final price: CAD ${l.format(v(t,i.checked))}`:"",o=parseFloat(y.value.replace(",",".")),s=o-n,h=((1-n/o)*100).toFixed(2),w=h>0?"\u2705 Good choice":"\u{1F6AB} Bad choice",F=o?`
     <hr>
     <li class="text-sm">${w}</li>
     <li>\u{1F914} Price in Brazil: ${p.format(o)}</li>
@@ -74,4 +74,4 @@
         ${r}
         <li class="text-xl font-bold">\u{1F1E7}\u{1F1F7} ${p.format(n)} \u{1FAE0}</li>
         ${F}
-    </ul>`,v()});L.addEventListener("click",e=>{v(),d.value="",y.value="",d.focus()});
+    </ul>`,b()});L.addEventListener("click",e=>{b(),d.value="",y.value="",d.focus()});
